2cb62494db377ea393b6e425953621a95d4fa527
Olivier Rey <rey.olivier@gmail.com>
2018-12-29 09:28:43 +0100

    introducting gml

--- rdf2gml.py
+++ rdf2gml.py
@@ -1,75 +1,60 @@
 #============================================
-# File name:      rdf2graphviz.py
+# File name:      rdf2gml.py
 # Author:         Olivier Rey
-# Date:           November 2018
+# Date:           December 2018
 # License:        GPL v3
+# Purpose:        Transforms rdf representations into gml
 #============================================
-
-import uuid, rdflib, sys, os, getopt
+import rdflib, getopt, sys, os
 
 from rdf_representations import *
 from utils import *
 
 from rdflib import Graph, Literal, BNode, RDF
 from rdflib.namespace import FOAF, DC
-from graphviz import Digraph
 
+
+def create_gml_node_string(id, name):
+    return 'node [\n  id ' + str(id) + '\n  label "' + name + '"\n]\n'
+
+
+def create_gml_rel_string(sourceid, targetid, name):
+    return 'edge [\n  label "' + name + '"\n' \
+           '  source ' + str(sourceid) + '\n  target '+ str(targetid) + '\n]\n'
     
-def print_rel_as_box(rel, dot):
-    ''''
-    This function is adding some extra representation to relationships.
-    '''
-    dot.node(rel.get_id(),rel.get_name(), shape='box')
-    dot.edge(rel.get_source_id(), rel.get_id())
-    dot.edge(rel.get_id(), rel.get_target_id())
-            
 
-def add_rdf_graph_to_dot(dot, rdfgraph, mode=0):
+def add_rdf_graph_to_gml(gmlfilename, rdfgraph):
     '''
-    mode=0 (default): prints labels in edges
-    mode=1: prints labels in boxes
-    mode=2: prints no labels
+    Creates a GML file from the RDF file with the same assumptions
+    than the graphviz visual representation.
+    Can be used with Gephi
     '''
     node_dict = {}
-    rel_dict  = {}
+    rel_dict = {}
+    f = open(gmlfilename, 'w')
+    f.write('graph [\n')
     for s, p, o in rdfgraph:
         source = add_to_nodes_dict(RDFNode(s),node_dict)
         target = add_to_nodes_dict(RDFNode(o),node_dict)
         add_to_rels_dict(RDFRel(p, source, target),rel_dict)
     for elem in node_dict.values():
-        dot.node(*elem.to_dot(), color="blue", fontcolor='blue')
-    if mode==1:
-        for elem in rel_dict.values():
-            print_rel_as_box(elem, dot)
-    elif mode==2:
-        for elem in rel_dict.values():
-            dot.edge(*elem.to_dot(False))
-    else:
-        for elem in rel_dict.values():
-            dot.edge(*elem.to_dot())
-    return dot
-
-
-    
-    
-def rdf_to_graphviz(store, name='default', mode=0):
-    dot = Digraph(comment=name, format='pdf')
-    dot.graph_attr['rankdir'] = 'LR'
-    add_rdf_graph_to_dot(dot, store, mode)
-    dot.render(name + '.dot', view=True)
-
+        f.write(create_gml_node_string(*elem.to_gml()))
+    for elem in rel_dict.values():
+        f.write(create_gml_rel_string(*elem.to_gml()))
+    f.write(']\n')
+    f.close()
 
 def usage():
-    print('RDF to GraphViz utility')
+    print('RDF to GML utility')
     print('Usage')
-    print('$ python3 rdf2graphviz.py -i [input_file_or_url] -o [output_dir] (other_options)')
+    print('$ python3 rdf2gml.py -i [input_file_or_url] -o [output_dir] (other_options)')
     print('-i or --input NAME: filename or URL')
     print('-o or --output NAME: directory name. Default will be "./tests/"')    
     print('Other options')
     print('-f or --format: "xml", "n3", "ntriples" or other format supported by Python rdflib. Default format is "n3".')
     print('-h or --help: usage')
 
-        
+    
 if __name__ == '__main__':
     try:
         opts, args = getopt.getopt(sys.argv[1:],"hi:o:f:v", [])
@@ -95,6 +80,7 @@ if __name__ == '__main__':
             myformat = a
         else:
             assert False, "unhandled option"
+
     # Check input
     if input == None:
         print("Input file or URL cannot be void.")
@@ -107,7 +93,8 @@ if __name__ == '__main__':
 
     store = Graph()
     result = store.parse(input, format=myformat)
-    dot = Digraph(comment="RDF to Graphviz: " + name)
-    dot.graph_attr['rankdir'] = 'LR'
-    add_rdf_graph_to_dot(dot, store, 1)
-    dot.render(os.path.join(outputdir,name + '.dot'), view=True)
+
+    #TODO replace with GML treatment
+    add_rdf_graph_to_gml(name + '.gml', store)
+    
+
