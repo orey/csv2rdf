286a5ed52064a62838f81e6020c4ad7c97e886c2
Olivier Rey <rey.olivier@gmail.com>
2020-08-09 09:03:16 +0200

    restructuring files

---
+++ install.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+
+pip3 install -r requirements.txt
---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(name='csv2rdf',
+    version='1.0',
+    description='CSV to RDF converter',
+    author='Olivier Rey',
+    author_email='rey.olivier@gmail.com',
+    url='https://github.com/orey/csv2rdf',
+    packages=['csv2rdf'],
+    install_requires=requirements,
+)
+
